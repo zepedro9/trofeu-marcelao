50ead86845fce1309dfd4c19c5cfe82303055ccb
Zé Pedro <nepsname@gmail.com>
2024-06-13 18:11:03 +0100

    Fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { app } from './firebaseConfig.js';
-import { getFirestore, query, collection, getDoc, getDocs, addDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, query, where, collection, getDoc, getDocs, addDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 document.addEventListener('DOMContentLoaded', () => {
